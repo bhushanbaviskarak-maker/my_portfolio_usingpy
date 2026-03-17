6c9f960cbd0e46127aeb797d7e86d2b9ef479fd8
bhushanbaviskarak-maker <bhushanbaviskarak@gmail.com>
2026-03-17 13:47:49 +0530

    Fix db for production

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+with app.app_context():
+    db.create_all()
+
 # ================================================
 # DATABASE MODELS
 # ================================================
